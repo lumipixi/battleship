50435519e1cdf362b52b37f020562e190284c158
lumipixi <98211231+lumipixi@users.noreply.github.com>
2023-05-24 23:28:23 -0600

    Receive and report attacks, hit ships, reject repeats

--- src/obj/GameboardFactory.js
+++ src/obj/GameboardFactory.js
@@ -1,5 +1,4 @@
 const GameboardFactory = () => {
-
   const board = [];
   for (let i = 0; i < 10; i++) {
     board.push(new Array(10).fill(0));
@@ -8,7 +7,18 @@ const GameboardFactory = () => {
   return {
     board,
     placeShip(ship, x, y) {
-      board[x].fill(ship, y, y + ship.length)
+      board[x].fill(ship, y, y + ship.length);
+    },
+
+    receiveAttack(x, y) {
+      if (board[x][y] === "X" || board[x][y] === "H") return "Repeated";
+
+      if (board[x][y] === 0) {
+        board[x][y] = "X";
+      } else {
+        board[x][y].hit();
+        board[x][y] = "H";
+      }
     },
   };
 };
--- src/obj/GameboardFactory.test.js
+++ src/obj/GameboardFactory.test.js
@@ -1,11 +1,49 @@
 import ShipFactory from "./ShipFactory";
-import GameboardFactory from "./GameboardFactory"
+import GameboardFactory from "./GameboardFactory";
 
 it("places ships on a given coordinate", () => {
   const ship = ShipFactory(5);
   const gameboard = GameboardFactory();
 
   gameboard.placeShip(ship, 2, 0);
-  expect(gameboard.board[2]).toEqual([ship, ship, ship, ship, ship, 0, 0, 0, 0, 0])
+  expect(gameboard.board[2]).toEqual([
+    ship,
+    ship,
+    ship,
+    ship,
+    ship,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
+});
 
-})
+it("successfully damages ships when attacked", () => {
+  const ship = ShipFactory(1);
+  const gameboard = GameboardFactory();
+
+  gameboard.placeShip(ship, 2, 0);
+
+  gameboard.receiveAttack(2, 0);
+  expect(gameboard.board[2][0]).toEqual("H");
+  expect(ship.isSunk()).toEqual(true);
+});
+
+it("successfully records missed attacks", () => {
+  const gameboard = GameboardFactory();
+
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.board[0][0]).toEqual("X");
+});
+
+it("successfully rejects missed attacks", () => {
+  const gameboard = GameboardFactory();
+
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.board[0][0]).toEqual("X");
+
+  const repeatedAttackOutcome = gameboard.receiveAttack(0, 0);
+  expect(repeatedAttackOutcome).toEqual("Repeated");
+});
